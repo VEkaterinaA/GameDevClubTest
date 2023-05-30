ef1e4956283dca024391c4adc15582b3dcef3645
Ekaterina <95909640+VEkaterinaA@users.noreply.github.com>
2023-05-30 11:40:55 +0300

    Created a hero prefab -> added start point for the hero.

---
+++ GameDevClubTest/Assets/Common/Resources/LocationInstaller.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using Zenject;
+
+public class LocationInstaller : MonoInstaller
+{
+    public Transform StartPoint;
+    public GameObject Hero;
+    public override void InstallBindings()
+    {
+    }
+}
